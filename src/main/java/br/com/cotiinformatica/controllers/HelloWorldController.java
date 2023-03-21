cd2349fabb6b486467952dcdc69fb4b9e4a26ed3
reblreis <rebreis88@gmail.com>
2023-03-20 22:48:14 -0300

    first commit

---
+++ src/main/java/br/com/cotiinformatica/controllers/HelloWorldController.java
@@ -0,0 +1,19 @@
+package br.com.cotiinformatica.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+@Api(tags = "Hello World!")
+@RestController
+public class HelloWorldController {
+
+	@ApiOperation("ENDPOINT para teste de mensagem.")
+	@GetMapping("/api/helloworld")
+	public String getMensagem() {
+		return "Olá, eu sou uma API!";
+	}
+
+}
